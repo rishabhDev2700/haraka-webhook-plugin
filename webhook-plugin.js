ef95ecaa18213e7dfc5e07ce12eb72b2be2fc1f4
rishabhdev2700 <rishavkumar2700@gmail.com>
2024-06-23 12:13:59 +0530

    Now you can send attachments as well

--- webhook-plugin.js
+++ webhook-plugin.js
@@ -14,37 +14,36 @@ exports.hook_data = function (next, connection) {
     next()
 }
 
-exports.hook_data_post = async function (next, connection) {
+exports.hook_queue = async function (next, connection) {
     const parser = new MailParser();
     const transaction = connection.transaction;
     transaction.message_stream.pipe(parser)
     let attachments = [];
     let content = ''
-    let count = 0
     parser.on('data', function (data) {
         if (data.type === 'text') {
             content = data.textAsHtml
         }
         if (data.type === 'attachment') {
-            count++;
             connection.loginfo("Got attachment:" + count)
             attachments.push(data);
             data.chunks = [];
             data.chunklen = 0;
-            let size = 0;
             data.content.on('readable', () => {
                 let chunk;
                 while ((chunk = data.content.read()) !== null) {
-                    size += chunk.length;
                     data.chunks.push(chunk);
                     data.chunklen += chunk.length;
                 }
             });
             data.content.on('end', () => {
                 data.buf = Buffer.concat(data.chunks, data.chunklen);
-                console.log('%s: %s B', 'size', size);
-                // attachment needs to be released before next chunk of
-                // message data can be processed
+                attachments.push({
+                    filename: data.filename,
+                    contentType: data.contentType,
+                    size: data.size,
+                    content: data.buf.toString('base64'),
+                })
                 data.release();
             });
 
@@ -52,41 +51,40 @@ exports.hook_data_post = async function (next, connection) {
         connection.loginfo("Data event");
     })
     parser.on("end", function () {
-        parser.updateImageLinks(
-            (attachment, done) => done(false, 'data:' + attachment.contentType + ';base64,' + attachment.buf.toString('base64')),
-            (err, html) => {
-                if (err) {
-                    connection.loginfo(err);
-                }
-                if (html) {
-                    connection.loginfo(html);
-                }
-            }
-        );
         connection.loginfo("\t\tEnd event!!\n\n")
         let emailData = {
             from: transaction.mail_from.address(),
             to: transaction.rcpt_to.map(rcpt => rcpt.address()),
             subject: transaction.header.get('subject'),
             body: content,
+            attachments: attachments.map(att => ({
+                filename: att.filename,
+                contentType: att.contentType,
+                size: att.size,
+                content: `data:${att.contentType};base64,${att.content}`
+            }))
         };
-        connection.loginfo(emailData);
-        axios.post('http://localhost:3000/email', emailData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': 'Node.js'
-            }
-        }).then(response => {
-            connection.loginfo(response)
-            connection.loginfo('+++++++++OK+++++++++++++')
-            next(OK);
+        try {
+            connection.loginfo(emailData);
+            const response = axios.post('http://localhost:3000/email', emailData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'Node.js'
+                }
+            })
+            connection.loginfo("\t--Sent!--");
+            connection.loginfo("Response:" + response.body)
         }
-        ).catch((err) => {
-            connection.loginfo("-------------------not OK--------------------");
-            next()
-        })
+        catch (e) {
+            connection.loginfo("\tError: " + e.message)
+        }
+        next(OK, `Message accepted with ID <${connection.uuid}>`)
     })
-    next();
+    parser.on('error', (err) => {
+        connection.logerror('Error parsing email');
+        connection.logerror(err.message);
+        next();
+    });
 
 }
 
